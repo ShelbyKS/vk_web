8ce25638eeb6683de90001ac742ddaefef52ea53
ShelbyKS <krylov.sanches@mail.ru>
2023-04-12 01:08:02 +0300

    added urls

--- ask_me/urls.py
+++ ask_me/urls.py
@@ -3,5 +3,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('hot/', include('main.urls')),
+    path('tag/tag1/', include('main.urls')),
+    path('question/q_number/', include('main.urls')),
+    path('login/', include('main.urls')),
+    path('signup/', include('main.urls')),
+    path('ask/', include('main.urls')),
 ]
--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.index), 
-   path('about', views.about)
+   path('', views.index),
+    path('hot/', views.hot_questions),
+    path('tag/tag1/', views.tag_questions),
+    path('question/q_number/', views.question),
+    path('login/', views.login),
+    path('signup/', views.signup),
+    path('ask/', views.ask_question),
 ]
